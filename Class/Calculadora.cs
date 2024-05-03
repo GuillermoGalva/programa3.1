609ab747c44449f1c48669bca02a7e4d117acd3d
guill <guill@GUILLERMO-PC>
2024-05-02 22:16:20 -0400

    Add project files.

---
+++ Class/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿
+namespace programa3._1.Class
+{
+ 
+        using System;
+
+    class Calculadora
+    {
+        public static void Calcular(double numero1, double numero2)
+        {
+            if (numero1 > numero2)
+            {
+                double suma = numero1 + numero2;
+                double diferencia = numero1 - numero2;
+                Console.WriteLine("La suma de los números es: " + suma);
+                Console.WriteLine("La diferencia de los números es: " + diferencia);
+            }
+            else
+            {
+                double producto = numero1 * numero2;
+                double division = numero1 / numero2;
+                Console.WriteLine("El producto de los números es: " + producto);
+                Console.WriteLine("La división del primer número respecto al segundo es: " + division);
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using programa3._1.Class;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        Calculadora.Calcular(numero1, numero2);
+
+        Console.ReadLine();
+    }
+}
